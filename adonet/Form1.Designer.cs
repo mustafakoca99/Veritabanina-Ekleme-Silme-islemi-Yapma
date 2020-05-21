7efa8cd035d0ca1bd430d78260eb6cbc2617225d
mustafakoca220@gmail.com <mustafakoca220@gmail.com>
2020-05-21 23:29:58 +0300

    Add project files.

---
+++ adonet/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace adonet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ad = new System.Windows.Forms.TextBox();
+            this.soyad = new System.Windows.Forms.TextBox();
+            this.bolumtext = new System.Windows.Forms.TextBox();
+            this.eklebuton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.listelebtn = new System.Windows.Forms.Button();
+            this.btnsil = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnguncelle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ad
+            // 
+            this.ad.Location = new System.Drawing.Point(122, 15);
+            this.ad.Name = "ad";
+            this.ad.Size = new System.Drawing.Size(243, 20);
+            this.ad.TabIndex = 0;
+            // 
+            // soyad
+            // 
+            this.soyad.Location = new System.Drawing.Point(122, 47);
+            this.soyad.Name = "soyad";
+            this.soyad.Size = new System.Drawing.Size(243, 20);
+            this.soyad.TabIndex = 1;
+            // 
+            // bolumtext
+            // 
+            this.bolumtext.Location = new System.Drawing.Point(122, 81);
+            this.bolumtext.Name = "bolumtext";
+            this.bolumtext.Size = new System.Drawing.Size(243, 20);
+            this.bolumtext.TabIndex = 2;
+            // 
+            // eklebuton
+            // 
+            this.eklebuton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.eklebuton.Location = new System.Drawing.Point(12, 159);
+            this.eklebuton.Name = "eklebuton";
+            this.eklebuton.Size = new System.Drawing.Size(353, 23);
+            this.eklebuton.TabIndex = 3;
+            this.eklebuton.Text = "EKLE";
+            this.eklebuton.UseVisualStyleBackColor = false;
+            this.eklebuton.Click += new System.EventHandler(this.eklebuton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(37, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "AD:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label2.Location = new System.Drawing.Point(37, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "SOYAD:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label3.Location = new System.Drawing.Point(36, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "BÖLÜM:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 276);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(353, 153);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // listelebtn
+            // 
+            this.listelebtn.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.listelebtn.Location = new System.Drawing.Point(12, 217);
+            this.listelebtn.Name = "listelebtn";
+            this.listelebtn.Size = new System.Drawing.Size(353, 23);
+            this.listelebtn.TabIndex = 8;
+            this.listelebtn.Text = "LİSTELE";
+            this.listelebtn.UseVisualStyleBackColor = false;
+            this.listelebtn.Click += new System.EventHandler(this.listelebtn_Click);
+            // 
+            // btnsil
+            // 
+            this.btnsil.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnsil.Location = new System.Drawing.Point(12, 188);
+            this.btnsil.Name = "btnsil";
+            this.btnsil.Size = new System.Drawing.Size(353, 23);
+            this.btnsil.TabIndex = 9;
+            this.btnsil.Text = "SİL";
+            this.btnsil.UseVisualStyleBackColor = false;
+            this.btnsil.Click += new System.EventHandler(this.btnsil_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(12, 107);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(353, 46);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "KONTROL";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(177, 18);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(170, 21);
+            this.comboBox1.TabIndex = 15;
+            this.comboBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.comboBox1_MouseClick_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label4.Location = new System.Drawing.Point(24, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "SEÇENEK(kullanmak isterse):";
+            // 
+            // btnguncelle
+            // 
+            this.btnguncelle.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnguncelle.Location = new System.Drawing.Point(12, 246);
+            this.btnguncelle.Name = "btnguncelle";
+            this.btnguncelle.Size = new System.Drawing.Size(353, 23);
+            this.btnguncelle.TabIndex = 13;
+            this.btnguncelle.Text = "GÜNCELLE";
+            this.btnguncelle.UseVisualStyleBackColor = false;
+            this.btnguncelle.Click += new System.EventHandler(this.Btnguncelle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 441);
+            this.Controls.Add(this.btnguncelle);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnsil);
+            this.Controls.Add(this.listelebtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.eklebuton);
+            this.Controls.Add(this.bolumtext);
+            this.Controls.Add(this.soyad);
+            this.Controls.Add(this.ad);
+            this.Name = "Form1";
+            this.Text = "Sorgu Ekranı";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ad;
+        private System.Windows.Forms.TextBox soyad;
+        private System.Windows.Forms.TextBox bolumtext;
+        private System.Windows.Forms.Button eklebuton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button listelebtn;
+        private System.Windows.Forms.Button btnsil;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnguncelle;
+    }
+}
+
---
+++ adonet/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace adonet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //bağlantı için kullanılan kodlar
+        SqlConnection connection = new SqlConnection("Server=.;Database=ekle; trusted_connection=true");
+        SqlCommand eklemek = new SqlCommand();
+        DataSet dataset = new DataSet();
+        SqlCommand sil = new SqlCommand();
+        SqlCommand guncelle = new SqlCommand();
+
+        private void eklebuton_Click(object sender, EventArgs e)
+        {
+            //bağlantı açtık
+            connection.Open();
+            try
+            {
+                //eklemek için kullanılacak komutlar
+                //parametre kullanarak ekleme işlemi yaptım
+                eklemek = connection.CreateCommand();
+                eklemek.CommandText = "Insert Into ekleisimsoyadbolum (ad,soyad,bolum) VALUES (@Ad,@Soyad,@Bolum)";
+                _ = eklemek.Parameters.Add("@Ad", ad.Text);
+                _ = eklemek.Parameters.Add("@Soyad", soyad.Text);
+                eklemek.Parameters.Add("@Bolum", bolumtext.Text);
+                //hata uyarı kodları
+                if (eklemek.ExecuteNonQuery()==1)
+                {
+                    MessageBox.Show("Ekleme işlemi başarılı", "Başarılı sonuç...");
+                }
+            }
+                //eklenmediği zaman uyarı verecek
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eklenemedi");
+            }
+            //bağlantı kapıyor
+            connection.Close();
+        }
+
+        private void listelebtn_Click(object sender, EventArgs e)
+        {
+            //veri varsa temizlemek için
+            dataset.Clear();
+            connection.Open();
+            //veritabanında bulunan verileri string ifadeleri göstermek için kullanır
+            string goster = "select*from ekleisimsoyadbolum";
+            //göster ile bağlantıyı birleştirir
+            SqlDataAdapter adapter = new SqlDataAdapter(goster,connection);
+            //dataseti veritabanı tablosuna ekler
+            adapter.Fill(dataset, "ekleisimsoyadbolum");
+            dataGridView1.DataSource = dataset;
+            //datagridviewde göstermek için
+            dataGridView1.DataMember = "ekleisimsoyadbolum";
+            //programda ki başlıkları düzelticek
+            dataGridView1.Columns["ad"].HeaderText = "Adı";
+            dataGridView1.Columns["soyad"].HeaderText = "Soyadı";
+            dataGridView1.Columns["bolum"].HeaderText = "Alanı";
+            //datagridview şekillendirme
+            dataGridView1.EnableHeadersVisualStyles = false;
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.Black;
+            dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.WhiteSmoke;
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("comic sans", 8, FontStyle.Bold); 
+            connection.Close();
+            dataset.Dispose();
+            adapter.Dispose();
+        }
+
+        private void btnsil_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            //silmek için kullanılan parametrik kodlar
+            sil = connection.CreateCommand();
+            sil.CommandText = "delete from ekleisimsoyadbolum where Ad=@Ad";
+            sil.Parameters.Add("@Ad", ad.Text);
+            //hata verdirme kodu
+            if (sil.ExecuteNonQuery()==1)
+            {
+                MessageBox.Show("Kayıt Silindi");
+            }
+            else
+	        {
+                    MessageBox.Show("Kayıt silme gerçekleşmedi");
+	        }
+            connection.Close();
+        }
+            //datagridview bilgileri üstüne tıklayınca textbox'ları doldurma
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int selectedValue = dataGridView1.CurrentRow.Index;
+            ad.Text = dataGridView1.Rows[selectedValue].Cells["ad"].Value.ToString();
+            soyad.Text = dataGridView1.Rows[selectedValue].Cells["soyad"].Value.ToString();
+            bolumtext.Text = dataGridView1.Rows[selectedValue].Cells["bolum"].Value.ToString();
+        }
+        //combobox'ı doldurma
+        private void comboBox1_MouseClick_1(object sender, MouseEventArgs e)
+        {
+            dataset.Clear();
+            connection.Open();
+            string sorgu = "select bolum from ekleisimsoyadbolum";
+            SqlDataAdapter mustafa = new SqlDataAdapter(sorgu, connection);
+            mustafa.Fill(dataset, "ekleisimsoyadbolum");
+            foreach (DataRow koca in dataset.Tables["ekleisimsoyadbolum"].Rows)
+            {
+                string deger = koca[0].ToString();
+                bool durum = comboBox1.Items.Contains(deger);
+                if (!durum)
+                {
+                    comboBox1.Items.Add(deger).ToString();
+                }
+                connection.Close();
+        }
+        }
+
+        private void Btnguncelle_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            //güncellemek için kullanılan parametrik kodlar
+            guncelle = connection.CreateCommand();
+            guncelle.CommandText = "UPDATE ekleisimsoyadbolum SET ad=@Ad,soyad=@Soyad,bolum=@Bolum where=id";
+            guncelle.Parameters.Add("@Ad", ad.Text);
+            guncelle.Parameters.Add("@Soyad", ad.Text);
+            guncelle.Parameters.Add("@Bolum", ad.Text);
+            //hata verdirme kodu
+            if (guncelle.ExecuteNonQuery() == 1)
+            {
+                MessageBox.Show("Kayıt Silindi");
+            }
+            else
+            {
+                MessageBox.Show("Kayıt silme gerçekleşmedi");
+            }
+            connection.Close();
+        }
+    }
+    }
+}
